4f432ae1c9f62f4871728cca657dec976714e896
Christopher Burr <christopher.burr@cern.ch>
2018-02-10 15:16:22 +0100

    Add LICENSE

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Licensed under a 3-clause BSD style license, see LICENSE.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
