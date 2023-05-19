329318ac5ee9e0228c275399ed1e279b40a45f53
Aryan Roy <aryanroy5678@gmail.com>
2023-05-19 10:52:14 +0530

    Add AST definition and two ways constructing ASTs

---
+++ src/formulate/AST.py
@@ -0,0 +1,65 @@
+"""
+Copyright (c) 2023 Aryan Roy. All rights reserved.
+
+formulate:  Easy conversions between different styles of expressions
+"""
+from __future__ import annotations
+
+import lark
+
+
+class AST:  # only three types (and a superclass to set them up)
+    _fields = ()
+
+    def __init__(self, *args, line=None):
+        self.line = line
+        for n, x in zip(self._fields, args):
+            setattr(self, n, x)
+            if self.line is None:
+                self.line = x.line
+
+
+class Literal(AST):  # Literal: value that appears in the program text
+    _fields = ("value",)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Symbol(AST):  # Symbol: value referenced by name
+    _fields = ("symbol",)
+
+    def __str__(self):
+        return self.symbol
+
+
+class UnaryOperator(AST):  # Unary Operator: Operation with one operand
+    _fields = ("sign", "operand")
+
+    def __str__(self):
+        return "{0}({1})".format(
+            str(self.function), ", ".join(str(x) for x in self.arguments)
+        )
+
+
+class BinaryOperator(AST):  # Binary Operator: Operation with two operands
+    _fields = ("sign", "left", "right")
+
+    def __str__(self):
+        return "{0}({1},{2})".format(str(self.sign), self.left, self.right)
+
+
+class Matrix(AST):  # Matrix: A matrix call
+    _fields = ("var", "paren")
+
+    def __str__(self):
+        return "{0}[{1}]".format(str(self.var), ", ".join(str(x) for x in self.paren))
+
+
+class Call(AST):  # Call: evaluate a function on arguments
+    _fields = ("function", "arguments")
+
+    def __str__(self):
+        return "{0}({1})".format(
+            str(self.function), ", ".join(str(x) for x in self.arguments)
+        )
---
+++ src/formulate/toast.py
@@ -0,0 +1,123 @@
+"""
+Copyright (c) 2023 Aryan Roy. All rights reserved.
+
+formulate:  Easy conversions between different styles of expressions
+"""
+from __future__ import annotations
+
+import lark
+from . import AST
+
+# from AST import Literal
+# from AST import BinaryOperator
+# from AST import UnaryOperator
+# from AST import Call
+# from AST import Symbol
+# from AST import Matrix
+
+
+UNARY_OP = {"pos", "neg", "binv", "linv"}
+with_sign = {}
+BINARY_OP = {
+    "add",
+    "sub",
+    "div",
+    "mul",
+    "lt",
+    "gt",
+    "lte",
+    "gte",
+    "eq",
+    "neq",
+    "band",
+    "bor",
+    "bxor",
+    "linv",
+    "land",
+    "lor",
+    "pow",
+}
+val_to_sign = {
+    "add": "+",
+    "sub": "-",
+    "div": "/",
+    "mul": "*",
+    "lt": "<",
+    "gt": ">",
+    "lte": "<=",
+    "gte": ">=",
+    "eq": "==",
+    "neq": "!=",
+    "band": "&",
+    "bor": "|",
+    "bxor": "^",
+    "linv": "!",
+    "land": "&&",
+    "lor": "||",
+    "neg": "-",
+    "pos": "+",
+    "binv": "~",
+    "linv": "!",
+    "pow": "**",
+}
+
+
+# def toast(ptnode):
+#     if ptnode.data in BINARY_OP and len(ptnode.children) == 2:
+#         arguments = [toast(x) for x in ptnode.children]
+#         return AST.BinaryOperator(
+#             AST.Symbol(val_to_sign[str(ptnode.data)], line=arguments[0].line), arguments[0], arguments[1]
+#         )
+#     elif ptnode.data == "pow" and len(ptnode.children) == 2:
+#         print("aefef")
+#         arguments = [toast(ptnode.children[0]), toast(ptnode.children[1])]
+#         return AST.Call(AST.Symbol("pow", line=arguments[0].line), arguments)
+#     elif ptnode.data == "call" and len(ptnode.children) == 2:
+#         return AST.Call(toast(ptnode.children[0]), toast(ptnode.children[1]))
+#     elif ptnode.data == "symbol":
+#         return AST.Symbol(str(ptnode.children[0]), line=ptnode.children[0].line)
+#     elif ptnode.data == "literal":
+#         return AST.Literal(float(ptnode.children[0]), line=ptnode.children[0].line)
+#     elif ptnode.data == "arglist":
+#         return [toast(x) for x in ptnode.children]
+#     else:
+#         return toast(
+#             ptnode.children[0]
+#         )  # many other cases, all of them simple pass-throughs
+
+
+def toast(ptnode):
+    data = ptnode.data
+
+    match data:
+        case "add":
+            if len(ptnode.children) == 2:
+                arguments = [toast(ptnode.children[0]), toast(ptnode.children[1])]
+                return AST.BinaryOperator(
+                    AST.Symbol("+", line=arguments[0].line), arguments[0], arguments[1]
+                )
+
+        case "sub":
+            if len(ptnode.children) == 2:
+                arguments = [toast(ptnode.children[0]), toast(ptnode.children[1])]
+                return AST.BinaryOperator(
+                    AST.Symbol("-", line=arguments[0].line), arguments[0], arguments[1]
+                )
+
+        case "matr":
+            children = ptnode.children
+            var = toast(children[0])
+            paren = []
+            children = children[1:]
+            for elem in children:
+                paren.append(toast(elem))
+            return AST.Matrix(var, paren)
+
+        case "symbol":
+            return AST.Symbol(str(ptnode.children[0]), line=ptnode.children[0].line)
+
+        case "literal":
+            return AST.Literal(float(ptnode.children[0]), line=ptnode.children[0].line)
+
+        case _:
+            return toast(ptnode.children[0])
