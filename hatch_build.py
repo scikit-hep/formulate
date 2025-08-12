ee77294367e7463ca61da3bfe7f31f34352f2869
Andres Rios Tascon <ariostas@gmail.com>
2025-08-12 16:42:59 -0400

    Fixed paths

--- hatch_build.py
+++ hatch_build.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import pathlib
+from pathlib import Path
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from lark import Lark
@@ -11,8 +11,8 @@ class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):  # noqa: ARG002
         parsers = ["numexpr", "ttreeformula"]
         for p in parsers:
-            # TODO: Check if we should be more careful with the path
-            grammar_path = pathlib.Path(f"src/formulate/{p}_grammar.lark")
-            parser_path = pathlib.Path(f"src/formulate/{p}_parser.py")
+            build_data["artifacts"].append(f"/src/formulate/{p}_parser.py")
+            grammar_path = Path(self.root, f"src/formulate/{p}_grammar.lark")
+            parser_path = Path(self.root, f"src/formulate/{p}_parser.py")
             parser = Lark(grammar_path.read_text(), parser="lalr")
             gen_standalone(parser, out=parser_path.open("w"))
